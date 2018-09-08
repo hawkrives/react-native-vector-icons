e3ba680690b5ef6a7b834e1b1ed4dd8cd3cb40cd
Hawken Rives <hawkrives@fastmail.fm>
2018-09-08 16:58:09 -0500

    add ionicons3 font

---
+++ Ionicons3.js
@@ -0,0 +1,17 @@
+/**
+ * Ionicons3 icon set component.
+ * Usage: <Ionicons3 name="icon-name" size={20} color="#4F8EF7" />
+ */
+
+import createIconSet from './lib/create-icon-set';
+import glyphMap from './glyphmaps/Ionicons3.json';
+
+const iconSet = createIconSet(glyphMap, 'Ionicons3', 'Ionicons3.ttf');
+
+export default iconSet;
+
+export const Button = iconSet.Button;
+export const TabBarItem = iconSet.TabBarItem;
+export const TabBarItemIOS = iconSet.TabBarItemIOS;
+export const ToolbarAndroid = iconSet.ToolbarAndroid;
+export const getImageSource = iconSet.getImageSource;
